7e8a20fed512809947a4b48a812f7c2559782e4a
Mohamed Hussein <husseeein91@gmail.com>
2022-10-19 10:50:51 +0200

    adding guards

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,11 +1,17 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/user/user.model';
 import { ConfigService } from '@nestjs/config';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(config: ConfigService) {
+  constructor(
+    config: ConfigService,
+    @InjectModel('User') private readonly userModel: Model<User>,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -13,7 +19,10 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     });
   }
 
-  validate(payload: { sub: number; email: string }) {
-    return { userId: payload.sub, email: payload.email };
+  async validate(payload: { sub: number; email: string }) {
+    const user = await this.userModel
+      .findById({ _id: payload.sub })
+      .select({ password: 0 });
+    return user;
   }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
 import { UserService } from './user.service';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
-  @UseGuards(AuthGuard('jwt'))
   @Get('')
   getAllUsers() {
     return this.userService.getAll();
+  }
+
+  @Get('me')
+  getCurrentUser(@Req() req: Request) {
+    return req.user;
   }
 }
